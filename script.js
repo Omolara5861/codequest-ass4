8224e5088cae0050557be2950c1d860abe03034d
Omolara5861 <debbiegterra@gmail.com>
2023-01-22 08:36:35 +0100

    refactor: :recycle: Covert inputed names to lowercase before checking

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const username = document.getElementById('username');
 const signUp = document.getElementById('sign-up');
 
-const adminUsers = [ 'Lara','Kennie','Precious','Karo'];
+const adminUsers = [ 'lara','kennie','precious','karo'];
 
 
 function onSignUp() {
-    const name = username.value;
-    // console.log(name);
-    if(name === adminUsers[0] 
+    const name = username.value.toLowerCase();
+
+    if(name === adminUsers[0]
         || name === adminUsers[1]
         || name === adminUsers[2]
         || name === adminUsers[3]) {
